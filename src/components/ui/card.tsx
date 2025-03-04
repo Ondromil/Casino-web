61ddb26f3ef0d6396fe93207d93e6bcc7cd4b1ef
Ondřej <ondrahlavac10@gmail.com>
2025-03-04 13:00:38 +0100

    a

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-
-export default function Card(props: any) {
+export default function Card(props) {
    return (
       <div className={`w-full h-[280px]  rounded-lg flex items-center justify-center ${props.bgImageClass} bg-cover rounded-xl`}>
             <h3 className="font-bold">{props.title}</h3>
