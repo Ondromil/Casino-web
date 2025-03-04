4a505737ade85c6fbdbfc9d9d0ff7e25283de6be
Ondřej <ondrahlavac10@gmail.com>
2025-03-04 13:06:03 +0100

    aa

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,4 +1,6 @@
-export default function Card(props) {
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
+export default function Card(props: any) {
    return (
       <div className={`w-full h-[280px]  rounded-lg flex items-center justify-center ${props.bgImageClass} bg-cover rounded-xl`}>
             <h3 className="font-bold">{props.title}</h3>
